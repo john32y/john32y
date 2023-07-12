b16ce82f3aa02747f197f226f7f36c0a2bc2ba66
john32y <john.thelronge@gmail.com>
2023-07-12 13:53:18 -0400

    WIP Next.js movie app

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+    return(
+        <main className="p-8">
+            <h1 className="text-2xl font-semibold text-orange-400">About</h1>
+            <p clasname="mt-4 text-zinc-100">Sake pase nap Bruille</p>
+
+        </main>
+    )
+}
